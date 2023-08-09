ee295659a83c17d79dcaf389d90e2797b6dce4fa
hossam <hadel@walaplus.com>
2023-08-10 00:30:10 +0300

    Make test pass for product service

--- products/src/services/product-service.test.js
+++ products/src/services/product-service.test.js
@@ -5,7 +5,7 @@ describe("ProductService",() => {
     describe("Signin",() => {
         // which scenarios we are testing
         test("valiadte user inputs",() => {});
-        test("validate response",() =>   expect(sum(1, 2)).toBe(4) );
+        test("validate response",() =>   expect(sum(1, 2)).toBe(3));
     });
 
 });
