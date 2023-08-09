315ef96b1bb2cc583ccf7e9a28dbc2474b2bbd52
hossam <hadel@walaplus.com>
2023-08-09 23:51:30 +0300

    Add test cases configuration

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,11 @@
+// which service it is
+describe("CustomerService",() => {
+
+    // which function you test
+    describe("Signin",() => {
+        // which scenarios we are testing
+        test("valiadte user inputs",() => {});
+        test("validate response",() => {});
+    });
+
+});
