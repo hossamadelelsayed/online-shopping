c50e27b5de4f0b7a608c43d9441e2dfad9cfa620
hossamadelelsayed <hossamadelelsayed@gmail.com>
2023-08-12 14:08:40 +0300

    Add test explanation (#1)
    * Add test explanation
    
    * Small change
    
    * Small change 2
    
    * Add test configuration for shopping and products services
    
    * Make wrong test for product service
    
    * Make test pass for product service
    
    * Make test pass for product service
    
    ---------
    
    Co-authored-by: hossam <hadel@walaplus.com>

--- products/src/services/product-service.test.js
+++ products/src/services/product-service.test.js
@@ -1,11 +1,11 @@
 // which service it is  
-describe("CustomerService",() => {
+describe("ProductService",() => {
 
     // sign in function test 
     describe("Signin",() => {
         // which scenarios we are testing
         test("valiadte user inputs",() => {});
-        test("validate response",() => {});
+        test("validate response",() => expect(3).toBe(3));
     });
 
 });
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,11 @@
+// which service it is  
+describe("ShoppingService",() => {
+
+    // sign in function test 
+    describe("Signin",() => {
+        // which scenarios we are testing
+        test("valiadte user inputs",() => {});
+        test("validate response",() => {});
+    });
+
+});
